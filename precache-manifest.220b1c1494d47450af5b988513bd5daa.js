6ec11ee4633b4b955c048a9a4b2861fd2d5018cd
fl <felix.laxvik@insurello.se>
2022-06-29 20:08:16 +0200

    beer browser

---
+++ precache-manifest.220b1c1494d47450af5b988513bd5daa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1435e2f01e244150a013649efeeb889d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "0fa0546148117f85f20ad79b3fdc21d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "0716ae5e6060e42829cb",
+    "url": "/static/css/main.5e0d2e2f.chunk.css"
+  },
+  {
+    "revision": "0716ae5e6060e42829cb",
+    "url": "/static/js/main.0716ae5e.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "354e092f0aa2734f8f4c",
+    "url": "/static/js/vendors~main.354e092f.chunk.js"
+  }
+]);
