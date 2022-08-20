793dc901dc8768425e07c5a4f8b9a6f4a9f0a17b
fexxor <>
2022-08-20 16:22:20 +0200

    yahtzee added

---
+++ yahtzee/precache-manifest.67b6f80052176485efb8cd9382a1c39d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "24bc68931befe309a04825058b3b5295",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b1f95132bb4a9d6b8380",
+    "url": "/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "b1f95132bb4a9d6b8380",
+    "url": "/static/js/main.b1f95132.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "bb36eb2f1e66572755c6",
+    "url": "/static/js/vendors~main.bb36eb2f.chunk.js"
+  }
+]);
